7f38aecd61c3e488eae88053438bc0246cf54eec
Kulsoom 2001 <209776085+KULSOOM2001@users.noreply.github.com>
2025-10-15 22:01:25 +0500

    Initial commit

---
+++ Model/Entities/Enrollment.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCD_EF_LAB.Model.Entities
+{
+    internal class Enrollment
+    {
+    }
+}
